9f8402154104a532cf396e81e8dd5ee4c329b706
sagacious123 <damilolaj23@gmail.com>
2024-12-07 00:42:45 +0100

    fixes

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: "http://127.0.0.1:5500",
+  origin: "*",
+  //   origin: "http://127.0.0.1:5500",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 
 // Middleware
